54727d5df5cad0991ddc6a89c8c0b57a062c7bcb
bhargavpk <bpk19900@gmail.com>
2020-11-20 16:52:12 +0530

    Testing user route

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,7 +18,7 @@ export default class Home extends Component {
         }
 
         componentDidMount(){
-                fetch('http://localhost:9000/users',{
+                fetch('/users',{
                         method:'GET'
                 }).then(res => res.json())
                 .then(data => {
