68ae4b65b040db25141c67ece481e663e921e63c
bhargavpk <bpk19900@gmail.com>
2020-11-20 17:45:41 +0530

    Client side routing modified

--- server/index.js
+++ server/index.js
@@ -18,8 +18,20 @@ if (process.env.NODE_ENV === 'production') {
         // Set static folder
         app.use(express.static('client/build'));
       
-        app.get('/*', (req, res) => {
-          res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        app.get('/', (req, res) => {
+          res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+        });
+
+        app.get('/transfer/:id', (req, res) => {
+                res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+        });
+
+        app.get('/account/:id', (req, res) => {
+                res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+        });
+
+        app.get('/transfers', (req, res) => {
+                res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
         });
 }
 
